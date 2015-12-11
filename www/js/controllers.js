096b016b0abd155ff7e177e62379209805e03034
Jean Lestang <jelestang@airfrance.fr>
2015-12-11 23:30:07 +0100

    Name of application

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('2048.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,4 @@
-angular.module('starter.services', [])
+angular.module('2048.services', [])
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
